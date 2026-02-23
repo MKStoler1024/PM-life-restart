2dfd2afb081babd8b8ed7c89d29e0900b26e59ff
MKStoler1024 <158786854+MKStoler1024@users.noreply.github.com>
2026-02-23 23:35:10 +0000

    feat: build的对github的特殊对待

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 /** @type {import('vite').UserConfig} */
+const basePath = process.env.VITE_BASE_PATH || '/';
+
 export default {
-  base: '/',
+  base: basePath,
   build: {
     outDir: 'dist',
   },
+  plugins: [
+    {
+      name: 'update-base-href',
+      transformIndexHtml(html) {
+        return html.replace(
+          /<base href="[^"]*"/,
+          `<base href="${basePath}"`
+        );
+      },
+    },
+  ],
 }
